52256c6ac01eca39b5b8ddd6457ceb778e878346
andyking <andrewk640@gmail.com>
2023-11-24 09:47:18 +0000

    commit from yesterday

---
+++ Day 4/dom excercise 2/index.js
@@ -0,0 +1,15 @@
+document.getElementById("redP").style.color = "red";
+
+const newDiv = document.getElementById("myDiv");
+
+const newPara = document.createElement("p");
+
+newPara.textContent = "Hello there!";
+
+newDiv.appendChild(newPara);
+
+const firstPara = document.querySelector("h2");
+
+firstPara.style.color = "red";
+
+
